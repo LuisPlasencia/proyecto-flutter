eba0437f97d79c69ebc334f0075c4b15f1178cff
Luis Plasencia <luisplasenciapulido@gmail.com>
2021-02-10 13:34:32 +0000

    boton inicio

--- telycom_app/lib/main.dart
+++ telycom_app/lib/main.dart
@@ -18,8 +18,14 @@ class FirstRoute extends StatelessWidget {
         child:
         Container(
           margin: const EdgeInsets.only(bottom: 80.0),
-          child: ElevatedButton(
-            child: Text('Entrar', ),
+          child: RaisedButton(
+            color: Colors.blue,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(18.0),
+                side: BorderSide(color: Colors.black)
+            ),
+            padding: EdgeInsets.only(left: 50, right:50, top: 10, bottom: 10),
+            child: Text('Entrar', style: TextStyle(fontSize: 26, color: Colors.white)),
             onPressed: () {
               Navigator.push(
                 context,
