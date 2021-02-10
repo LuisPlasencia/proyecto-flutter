118e4d06f9620c51219ba884bb585255c778a67b
cysersery <cyserser@gmail.com>
2021-02-10 13:32:06 +0000

    T0.2 ListView

--- telycom_app/lib/main.dart
+++ telycom_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+
 void main() {
   runApp(MaterialApp(
     title: 'Navigation Basics',
@@ -33,21 +34,64 @@ class FirstRoute extends StatelessWidget {
   }
 }
 
+class ElementList {
+  String _creation;
+  String _reference;
+  String _state;
+  String _direction;
+
+  ElementList(this._creation, this._reference, this._state, this._direction);
+
+  String get creation => _creation;
+  set creation(String value) {
+    _creation = value;
+  }
+
+  String get reference => _reference;
+  set reference(String value) {
+    _reference = value;
+  }
+
+  String get state => _state;
+  set state(String value) {
+    _state = value;
+  }
+
+  String get direction => _direction;
+  set direction(String value) {
+    _direction = value;
+  }
+}
+
+List<ElementList> itemsList = [
+ElementList('12:45 05/02/21', 'LPA21/0011', 'No atendido', 'Las Palmas G.C.'),
+ElementList('12:45 05/02/21', 'LPA21/0011', 'Atendido', 'Las Palmas G.C.'),
+ElementList('12:45 05/02/21', 'LPA21/0021', 'No atendido', 'Las Palmas G.C.'),
+];
+
 class SecondRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Second Route"),
+        title: Text("Mis incidencias"),
       ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: Text('Go back!'),
-        ),
-      ),
+      body: ListView.builder(
+        itemCount: itemsList.length,
+        itemBuilder: (context, index){
+          return Card(
+            child: ListTile(
+              onTap: (){},
+                title: Text(itemsList[index]._direction),
+              leading: CircleAvatar(
+                backgroundImage: AssetImage(
+                    'images/bear.png'
+                ),
+              ),
+            ),
+          );
+        },
+      )
     );
   }
 }
