d8f391363010feb498dba96af2894b5b0a877f8d
Luis Plasencia <luisplasenciapulido@gmail.com>
2021-02-11 12:52:54 +0000

    ajustando la imagen

--- telycom_app/lib/misIncidencias.dart
+++ telycom_app/lib/misIncidencias.dart
@@ -95,54 +95,57 @@ class _MisIncidenciasState extends State<MisIncidencias> {
                 ],
               ),
             ),
-                Flexible(
-                  child: FlutterMap(
-                    options: MapOptions(
-                      center: LatLng(28.0713516, -15.45598),
-                      zoom: 11.0,
-                      plugins: [EsriPlugin()],
+                Container(
+                  height: 470,
+                  child: Flexible(
+                    child: FlutterMap(
+                      options: MapOptions(
+                        center: LatLng(28.0713516, -15.45598),
+                        zoom: 11.0,
+                        plugins: [EsriPlugin()],
 
+                      ),
+                      layers: [
+                        TileLayerOptions(
+                          urlTemplate:
+                          'http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',
+                          subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+                          tileProvider: CachedNetworkTileProvider(),
+                        ),
+                        FeatureLayerOptions(
+                          url: "https://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/USA_Congressional_Districts/FeatureServer/0",
+                          geometryType:"polygon",
+                          onTap: (attributes, LatLng location) {
+                            print(attributes);
+                          },
+                          render: (dynamic attributes){
+                            // You can render by attribute
+                            return PolygonOptions(
+                                borderColor: Colors.blueAccent,
+                                color: Colors.black12,
+                                borderStrokeWidth: 2
+                            );
+                          },
+
+                        ),
+                        FeatureLayerOptions(
+                          url: "https://services8.arcgis.com/1p2fLWyjYVpl96Ty/arcgis/rest/services/Forest_Service_Recreation_Opportunities/FeatureServer/0",
+                          geometryType:"point",
+                          render:(dynamic attributes){
+                            // You can render by attribute
+                            return Marker(
+                              width: 30.0,
+                              height: 30.0,
+                              builder: (ctx) => Icon(Icons.pin_drop),
+                            );
+                          },
+                          onTap: (attributes, LatLng location) {
+                            print(attributes);
+                          },
+                        ),
+
+                      ],
                     ),
-                    layers: [
-                      TileLayerOptions(
-                        urlTemplate:
-                        'http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',
-                        subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
-                        tileProvider: CachedNetworkTileProvider(),
-                      ),
-                      FeatureLayerOptions(
-                        url: "https://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/USA_Congressional_Districts/FeatureServer/0",
-                        geometryType:"polygon",
-                        onTap: (attributes, LatLng location) {
-                          print(attributes);
-                        },
-                        render: (dynamic attributes){
-                          // You can render by attribute
-                          return PolygonOptions(
-                              borderColor: Colors.blueAccent,
-                              color: Colors.black12,
-                              borderStrokeWidth: 2
-                          );
-                        },
-
-                      ),
-                      FeatureLayerOptions(
-                        url: "https://services8.arcgis.com/1p2fLWyjYVpl96Ty/arcgis/rest/services/Forest_Service_Recreation_Opportunities/FeatureServer/0",
-                        geometryType:"point",
-                        render:(dynamic attributes){
-                          // You can render by attribute
-                          return Marker(
-                            width: 30.0,
-                            height: 30.0,
-                            builder: (ctx) => Icon(Icons.pin_drop),
-                          );
-                        },
-                        onTap: (attributes, LatLng location) {
-                          print(attributes);
-                        },
-                      ),
-
-                    ],
                   ),
                 ),
               ],
