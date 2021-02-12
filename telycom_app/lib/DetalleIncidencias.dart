baa3a4eba703daf69a89a7e6158212b785f99adb
cysersery <cyserser@gmail.com>
2021-02-12 12:46:22 +0000

    Añdiendo mas cositas en el detalle + botones :D

--- telycom_app/lib/DetalleIncidencias.dart
+++ telycom_app/lib/DetalleIncidencias.dart
@@ -27,104 +27,233 @@ class DetalleInicidencias extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text(reference)),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              width: double.infinity,
-              child: Card(
-                color: Colors.grey,
-                child: Padding(
-                  padding: EdgeInsets.only(left: 10.0),
-                  child: Text(
-                    "Descripción",
-                    style: TextStyle(fontSize: 20),
-                    textAlign: TextAlign.left,
-                  ),
+      appBar: AppBar(title: Text(reference)),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.grey,
+              child: Padding(
+                padding: EdgeInsets.only(left: 10.0),
+                child: Text(
+                  "Descripción",
+                  style: TextStyle(fontSize: 20),
+                  textAlign: TextAlign.left,
                 ),
               ),
             ),
-            Padding(
-              padding: EdgeInsets.only(left: 10.0),
-              child: Text(
-                description,
-                style: TextStyle(fontSize: 15),
-                textAlign: TextAlign.right,
-              ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 10.0),
+            child: Text(
+              description,
+              style: TextStyle(fontSize: 15),
+              textAlign: TextAlign.right,
             ),
-
-            Container(
-              width: double.infinity,
-              child: Card(
-                color: Colors.grey,
-                child: Padding(
-                  padding: EdgeInsets.only(left: 10.0),
-                  child: Text(
-                    "Código",
-                    style: TextStyle(fontSize: 20),
-                    textAlign: TextAlign.left,
-                  ),
+          ),
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.grey,
+              child: Padding(
+                padding: EdgeInsets.only(left: 10.0),
+                child: Text(
+                  "Código",
+                  style: TextStyle(fontSize: 20),
+                  textAlign: TextAlign.left,
                 ),
               ),
             ),
-            Padding(
-              padding: EdgeInsets.only(left: 10.0),
-              child: Text(
-                reference,
-                style: TextStyle(fontSize: 15),
-                textAlign: TextAlign.right,
-              ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 10.0),
+            child: Text(
+              reference,
+              style: TextStyle(fontSize: 15),
+              textAlign: TextAlign.right,
             ),
-
-            Container(
-              width: double.infinity,
-              child: Card(
-                color: Colors.grey,
-                child: Padding(
-                  padding: EdgeInsets.only(left: 10.0),
-                  child: Text(
-                    "Creación",
-                    style: TextStyle(fontSize: 20),
-                    textAlign: TextAlign.left,
-                  ),
+          ),
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.grey,
+              child: Padding(
+                padding: EdgeInsets.only(left: 10.0),
+                child: Text(
+                  "Creación",
+                  style: TextStyle(fontSize: 20),
+                  textAlign: TextAlign.left,
                 ),
               ),
             ),
-            Padding(
-              padding: EdgeInsets.only(left: 10.0),
-              child: Text(
-                creation,
-                style: TextStyle(fontSize: 15),
-                textAlign: TextAlign.right,
-              ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 10.0),
+            child: Text(
+              creation,
+              style: TextStyle(fontSize: 15),
+              textAlign: TextAlign.right,
             ),
-
-            Container(
-              width: double.infinity,
-              child: Card(
-                color: Colors.grey,
-                child: Padding(
-                  padding: EdgeInsets.only(left: 10.0),
-                  child: Text(
-                    "Dirección",
-                    style: TextStyle(fontSize: 20),
-                    textAlign: TextAlign.left,
-                  ),
+          ),
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.grey,
+              child: Padding(
+                padding: EdgeInsets.only(left: 10.0),
+                child: Text(
+                  "Dirección",
+                  style: TextStyle(fontSize: 20),
+                  textAlign: TextAlign.left,
                 ),
               ),
             ),
-            Padding(
-              padding: EdgeInsets.only(left: 10.0),
-              child: Text(
-                direction,
-                style: TextStyle(fontSize: 15),
-                textAlign: TextAlign.right,
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 10.0),
+            child: Text(
+              direction,
+              style: TextStyle(fontSize: 15),
+              textAlign: TextAlign.right,
+            ),
+          ),
+          Divider(
+            thickness: 3,
+            color: Colors.black,
+          ),
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.blueAccent,
+              child: Padding(
+                padding: EdgeInsets.only(left: 10.0),
+                child: Text(
+                  "09:56\n09/02/21 " + " Suceso atendido por TELYCOM1",
+                  style: TextStyle(fontSize: 16),
+                  textAlign: TextAlign.left,
+                ),
               ),
             ),
+          ),
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.blueAccent,
+              child: Padding(
+                padding: EdgeInsets.only(left: 10.0),
+                child: Row(
+                  children: [
+                    RichText(
+                      text: TextSpan(
+                        children: <TextSpan>[
+                          TextSpan(
+                            text: "11:23\n09/02/21",
+                            style: TextStyle(fontSize: 16),
+                          ),
+                        ],
+                      ),
+                    ),
 
-          ],
-        )
+                    SizedBox(width: 20),
+
+                    RichText(
+                      text: TextSpan(
+                        children: <TextSpan>[
+                          TextSpan(
+                            text: "Incidente creado por TELYCOM1",
+                            style: TextStyle(fontSize: 16),
+                          ),
+
+                          TextSpan(
+                            text: "\nTipificación:",
+                            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold ),
+                          ),
+                          TextSpan(
+                            text: "\n10:30 Tráfico DEFAULT",
+                            style: TextStyle(fontSize: 16),
+                          ),
+
+                          TextSpan(
+                            text: "\nLocalización:",
+                            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold ),
+                          ),
+                          TextSpan(
+                            text: "\nLas Palmas de Gran Canaria",
+                            style: TextStyle(fontSize: 16),
+                          ),
+
+                          TextSpan(
+                            text: "\nAgencia asignada:",
+                            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold ),
+                          ),
+                          TextSpan(
+                            text: "\nPolicia Local",
+                            style: TextStyle(fontSize: 16),
+                          ),
+
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+
+                // Text(
+                //   "11:23\n09/02/21 "+" Incidente creado por TELYCOM1",
+                //   style: TextStyle(fontSize: 16),
+                //   textAlign: TextAlign.left,
+                // ),
+              ),
+            ),
+          ),
+          Spacer(),
+          Row(
+            children: [
+              Card(
+                color: Colors.grey,
+                child: Container(
+                    padding: EdgeInsets.all(20.0),
+                    child: Image(
+                      image: AssetImage('images/email.png'),
+                      width: 50,
+                      height: 50,
+                    )),
+              ),
+              Card(
+                color: Colors.grey,
+                child: Container(
+                    padding: EdgeInsets.all(20.0),
+                    child: Image(
+                      image: AssetImage('images/image.png'),
+                      width: 50,
+                      height: 50,
+                    )),
+              ),
+              Card(
+                color: Colors.grey,
+                child: Container(
+                    padding: EdgeInsets.all(20.0),
+                    child: Image(
+                      image: AssetImage('images/placeholder.png'),
+                      width: 50,
+                      height: 50,
+                    )),
+              ),
+              Card(
+                color: Colors.red[400],
+                child: Container(
+                    padding: EdgeInsets.all(20.0),
+                    child: Image(
+                      image: AssetImage('images/log-out.png'),
+                      width: 50,
+                      height: 50,
+                    )),
+              ),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
