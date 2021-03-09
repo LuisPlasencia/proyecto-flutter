8b35cf1c4fd6fdc07f8ea1728b4f2b06cc57799e
Luis Plasencia <luisplasenciapulido@gmail.com>
2021-03-09 09:30:16 +0000

    quitando porqueria

--- telycom_app/lib/main.dart
+++ telycom_app/lib/main.dart
@@ -4,14 +4,7 @@ import 'AuthCall.dart';
 import 'Token.dart';
 import "misIncidencias.dart";
 import 'package:flutter/services.dart';
-
-
-
 import 'dart:async';
-import 'dart:convert';
-
-import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 
 void main() => runApp(MyApp());
 
@@ -34,7 +27,6 @@ class FirstRoute extends StatefulWidget {
 class _FirstRouteState extends State<FirstRoute> {
 
   final TextEditingController textFieldController = TextEditingController();
-  final TextEditingController textFieldController2 = TextEditingController();
   Future<Token> futureToken;
   String usuario = "lolo";
   bool cargando = false;
@@ -80,95 +72,91 @@ class _FirstRouteState extends State<FirstRoute> {
             ),
 
 
-            Column(
-              children: [
-                Container(
-                  margin: EdgeInsets.only(bottom: 10.0, left: 50.0, right: 50.0),
-                  child: TextField(
-                      controller:textFieldController,
-                      //la llave es necesaria para realizar el test de integración
-                      key: Key('user'),
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'Usuario',
-                      ),
-
-                      style:TextStyle(
-                        fontSize: 20,
-                        color:Colors.black,
-                      )
+            Container(
+              margin: EdgeInsets.only(bottom: 10.0, left: 50.0, right: 50.0),
+              child: TextField(
+                  controller:textFieldController,
+                  //la llave es necesaria para realizar el test de integración
+                  key: Key('user'),
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Usuario',
                   ),
-                ),
 
-                Container(
-                  margin: EdgeInsets.only(top: 10.0, left: 50.0, right: 50.0),
-
-                  child: TextField(
-                      obscureText: true,
-                      controller:textFieldController2,
-                      key: Key('password'),
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'Contraseña',
-                      ),
-
-                      style:TextStyle(
-                        fontSize: 20,
-                        color:Colors.black,
-                      )
-                  ),
-                ),
-              ],
+                  style:TextStyle(
+                    fontSize: 20,
+                    color:Colors.black,
+                  )
+              ),
             ),
 
-            cargando ? FutureBuilder<Token>(
-              future: futureToken,
-              builder: (context, snapshot) {
-                if (snapshot.hasData) {
-                  if(snapshot.data.statusTelyAPI == '200' || snapshot.data.statusTelyAPI == '202' || snapshot.data.statusTelyAPI == '203'){
-                    final snackbar = SnackBar(
-                        backgroundColor: Colors.yellow,
-                        content: Text(
-                          "Error: " + snapshot.data.statusTelyAPI,
-                          style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold),
-                        ));
 
-                    WidgetsBinding.instance.addPostFrameCallback((_){
-                      // Add Your Code here.
-                      ScaffoldMessenger.of(context).showSnackBar(snackbar);
-                    });
-                    return SizedBox();
+              // Comprobando el loading
+              cargando ? FutureBuilder<Token>(
+                      future: futureToken,
+                      builder: (context, snapshot) {
+                        if (snapshot.hasData) {
+                          if (snapshot.data.statusTelyAPI == '200' ||
+                              snapshot.data.statusTelyAPI == '202' ||
+                              snapshot.data.statusTelyAPI == '203') {
+                            WidgetsBinding.instance.addPostFrameCallback((_) {
+                              Navigator.push(context, MaterialPageRoute(
+                                    builder: (context) => MisIncidencias()),);
+                            });
+                            return new Container(
+                              height: 100,
+                              padding: EdgeInsets.only(bottom: 15),
+                            );
+                          } else {
+                            final snackbar = SnackBar(
+                                backgroundColor: Colors.yellow,
+                                content: Text(
+                                  "Error: " + snapshot.data.statusTelyAPI,
+                                  style: TextStyle(
+                                      color: Colors.red,
+                                      fontWeight: FontWeight.bold),
+                                ));
 
-                  } else{
-                    final snackbar = SnackBar(
-                        backgroundColor: Colors.yellow,
-                        content: Text(
-                          "Error: " + snapshot.data.statusTelyAPI,
-                          style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold),
-                        ));
-
-                    ScaffoldMessenger.of(context).showSnackBar(snackbar);
-                  }
-
-                } else if (snapshot.hasError) {
-                  final snackbar = SnackBar(
-                      backgroundColor: Colors.yellow,
-                      content: Text(
-                        snapshot.error.toString(),
-                        style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold),
-                      ));
-
-                  // ScaffoldMessenger.of(context).showSnackBar(snackbar);
-
-                  return Text("${snapshot.error}");
-                }
-                // By default, show a loading spinner.
-                return Container(
-                    padding: EdgeInsets.only(bottom:15),
-                    child: SpinKitHourGlass(color: Colors.black));
-              },
-            ) : SizedBox(),
-
+                            WidgetsBinding.instance.addPostFrameCallback((_) {
+                              // Add Your Code here.
+                              ScaffoldMessenger.of(context)
+                                  .showSnackBar(snackbar);
+                            });
+                            return new Container(
+                              height: 100,
+                              padding: EdgeInsets.only(bottom: 15),
+                            );
+                          }
+                        } else if (snapshot.hasError) {
+                          final snackbar = SnackBar(
+                              backgroundColor: Colors.yellow,
+                              content: Text(
+                                snapshot.error.toString(),
+                                style: TextStyle(
+                                    color: Colors.red,
+                                    fontWeight: FontWeight.bold),
+                              ));
+                          // ScaffoldMessenger.of(context).showSnackBar(snackbar);
+                          WidgetsBinding.instance.addPostFrameCallback((_) {
+                            // Add Your Code here.
+                            ScaffoldMessenger.of(context).showSnackBar(snackbar);
+                          });
+                          return new Container(
+                            height: 100,
+                            padding: EdgeInsets.only(bottom: 15),
+                          );
+                        }
+                        // By default, show a loading spinner.
+                        return Container(
+                            height: 100,
+                            padding: EdgeInsets.only(bottom: 15),
+                            child: SpinKitHourGlass(color: Colors.black));
+                      },
+                    )
+                  : new Container(
+                        height: 100,
+                        padding: EdgeInsets.only(bottom: 15),
+                    ),
 
             // FutureBuilder<Token>(
             //   future: futureToken,
@@ -186,10 +174,6 @@ class _FirstRouteState extends State<FirstRoute> {
             //         child: SpinKitHourGlass(color: Colors.white));
             //   },
             // ),
-
-
-
-
 
             Container(
               // margin: const EdgeInsets.only(bottom: 80.0),
