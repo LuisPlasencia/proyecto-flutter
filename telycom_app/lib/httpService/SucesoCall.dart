2ba70b6c786b752fd756dd17609a6a24f506692c
Luis Plasencia <luisplasenciapulido@gmail.com>
2021-03-18 09:46:51 +0000

    Retornando lista de incidencias en la llamada

--- telycom_app/lib/httpService/SucesoCall.dart
+++ telycom_app/lib/httpService/SucesoCall.dart
@@ -10,20 +10,16 @@ class SucesoCall{
   static const String dominioTelyApiServer = '192.168.15.38';
   static const String imei = '987654321';
 
-  static Future<Suceso> fetchSuceso(String token) async {
+  static Future<List<Suceso>> fetchSuceso(String token) async {
     String url = "http://$dominioTelyApiServer/TelyGIS/AndroidServlet?tk=$token&q=getsucesos&imei=$imei";
-    String tipo;
-    String idSuceso;
-    String refSuceso;
-    String description;
-    String latitude;
-    String longitude;
+    List<Suceso> list_sucesos;
 
+    var tipo;
     var idSucesoArray;
     var refSucesoArray;
-    var descriptionArray;
     var latitudeArray;
     var longitudeArray;
+    List<String> descriptionArray;
 
     final response = await http.get(Uri.parse(url));
 
@@ -36,45 +32,72 @@ class SucesoCall{
     latitudeArray = datos[4].split(';');
     longitudeArray = datos[5].split(';');
 
+    // //Excepción en la que hay descripción vacía de una incidencia
+    // RegExp exp = new RegExp('.s');
+    // Iterable<RegExpMatch> matches = exp.allMatches(datos[3]);
+    //
+    //
+    // if(datos[3][datos[3].length] == ';'){
+    //   datos[3] = datos[3] + "none";
+    //   descriptionArray = datos[3].split(';');
+    // }else{
+    //   descriptionArray = datos[3].split(';');
+    // }
+    // descriptionArray = datos[3].split(';', -1);
+    //
+    // int posicion = 0;
+    // while(datos[3].indexOf(';',posicion) != -1){
+    //   posicion = datos[3].indexOf(';',posicion);
+    //
+    // }
+    //
+    // descriptionArray = List<String>.filled(idSucesoArray.length, "");
+    // int posicion = 0;
+    // for(int i=0; i<datos[3].length; i++){
+    //   posicion = descriptionArray[0].indexOf(';', posicion);
+    //
+    // }
 
 
-    Suceso suceso = new Suceso(tipo, idSucesoArray[3].toString(), "refSuceso", "description"
-        , 20, 23);
+    for(int i = 0; i<idSucesoArray.length; i++){
+      Suceso suceso = new Suceso(tipo, idSucesoArray[i], refSucesoArray[i], descriptionArray[i], latitudeArray[i], longitudeArray[i]);
+      list_sucesos.add(suceso);
+    }
 
-      switch (response.statusCode) {
-        case 200:
-          return suceso;
-          break;
-        case 202:
-          return suceso;
-          break;
-        case 203:
-          return suceso;
-          break;
-        case 451:
-          throw Exception('451: Petición no válida');
-          break;
-        case 460:
-          throw Exception('460: Licencia no válida');
-          break;
-        case 461:
-          throw Exception('461: Error de licencia');
-          break;
-        case 462:
-          throw Exception('462: Nº de peticiones superado');
-          break;
-        case 464:
-          throw Exception('464: La licencia no admite usuarios de terceros');
-          break;
-        case 552:
-          throw Exception('552: Error de acceso a base de datos');
-          break;
-        case 553:
-          throw Exception('553: Error de acceso a base de datos');
-          break;
-        default:
-          throw Exception('Failed to load token');
-      }
+    switch (response.statusCode) {
+      case 200:
+        return list_sucesos;
+        break;
+      case 202:
+        return list_sucesos;
+        break;
+      case 203:
+        return list_sucesos;
+        break;
+      case 451:
+        throw Exception('451: Petición no válida');
+        break;
+      case 460:
+        throw Exception('460: Licencia no válida');
+        break;
+      case 461:
+        throw Exception('461: Error de licencia');
+        break;
+      case 462:
+        throw Exception('462: Nº de peticiones superado');
+        break;
+      case 464:
+        throw Exception('464: La licencia no admite usuarios de terceros');
+        break;
+      case 552:
+        throw Exception('552: Error de acceso a base de datos');
+        break;
+      case 553:
+        throw Exception('553: Error de acceso a base de datos');
+        break;
+      default:
+        throw Exception('Failed to load token');
+    }
 
 
   }
